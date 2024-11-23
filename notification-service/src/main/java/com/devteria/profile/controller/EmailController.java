71d22abf55822423056be235740fe2411154c43b
Hai Huynh <info.haihuynh@gmail.com>
2024-11-23 13:02:13 +0700

    feat: install, configuration kafka with docker

--- notification-service/src/main/java/com/devteria/profile/controller/EmailController.java
+++ notification-service/src/main/java/com/devteria/profile/controller/EmailController.java
@@ -1,5 +1,6 @@
 package com.devteria.profile.controller;
 
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,12 @@ import com.devteria.profile.service.EmailService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class EmailController {
     EmailService emailService;
 
@@ -25,5 +28,10 @@ public class EmailController {
                 .result(emailService.sendEmail(request))
                 .build();
         return response;
+    }
+
+    @KafkaListener(topics = "onboard-successful")
+    public void listener(String message) {
+        log.info("received message: " + message);
     }
 }
